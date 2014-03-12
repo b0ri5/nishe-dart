a12e670b3f3db12844e03d9a31845c4a854f8fcf
Greg Tener <borisb0ri5@gmail.com>
2014-03-11 17:27:58 -0700

    Flesh out partition and its tests.

--- lib/nishe.dart
+++ lib/nishe.dart
@@ -1,1 +1,43 @@
 library nishe;
+
+import 'dart:collection';
+
+class Partition {
+  final Map _images;
+
+  Partition._internal(this._images);
+
+  Partition(List<List> partition): this._internal(_partitionToImages(partition)
+      );
+
+  static Map _partitionToImages(List<List> partition) {
+    var domain = new List();
+    for (var cell in partition) {
+      if (cell.length == 0) {
+        throw new ArgumentError("cells must have length > 0");
+      }
+      for (var x in cell) {
+        if (domain.contains(x)) {
+          throw new ArgumentError("the element $x was seen more than once");
+        }
+        domain.add(x);
+      }
+    }
+    domain.sort();
+    var images = new Map();
+    int index = 0;
+    for (var cell in partition) {
+      for (var x in cell) {
+        images[x] = domain[index];
+      }
+      index += cell.length;
+    }
+    return images;
+  }
+
+  image(x) => _images[x];
+  Set get domain {
+    var domain = new SplayTreeSet()..addAll(_images.keys);
+    return domain;
+  }
+}
--- test/nishe_test.dart
+++ test/nishe_test.dart
@@ -1,1 +1,56 @@
 import 'package:unittest/unittest.dart';
+import 'package:nishe-dart/nishe.dart';
+
+void main() {
+  group('Partition', () {
+    group('#fromLists', () {
+      test('makes sense for empty partition', () {
+        expect(new Partition([]).domain, isEmpty);
+      });
+    });
+    group('#image', () {
+      test('matches input', () {
+        expect(new Partition([['a']]).image('a'), equals('a'));
+        expect(new Partition([['b']]).image('b'), equals('b'));
+
+        var p1 = new Partition([['a', 'b']]);
+        expect(p1.image('a'), equals('a'));
+        expect(p1.image('b'), equals('a'));
+
+        var p2 = new Partition([['a'], ['b']]);
+        expect(p2.image('a'), equals('a'));
+        expect(p2.image('b'), equals('b'));
+      });
+      test('malformed input', () {
+        expect(() => new Partition([[]]), throwsArgumentError);
+        expect(() => new Partition([['a'], []]), throwsArgumentError);
+        expect(() => new Partition([['a'], ['a']]), throwsArgumentError);
+        expect(() => new Partition([['a', 'a']]), throwsArgumentError);
+      });
+      test('modifications do not modify partition', () {
+        var list = [['a']];
+        var p = new Partition(list);
+        list[0][0] = 'b';
+        expect(p.image('a'), equals('a'));
+        expect(p.domain, equals(['a']));
+      });
+    });
+    group('#domain', () {
+      test('matches input', () {
+        expect(new Partition([['a']]).domain, equals(['a']));
+        expect(new Partition([['b']]).domain, equals(['b']));
+        expect(new Partition([['a', 'b']]).domain, equals(['a', 'b']));
+        expect(new Partition([['b', 'a']]).domain, equals(['a', 'b']));
+        expect(new Partition([['a'], ['b']]).domain, equals(['a', 'b']));
+        expect(new Partition([['b'], ['a']]).domain, equals(['a', 'b']));
+      });
+      test('modifications do not affect partition', () {
+        var p = new Partition([['a']]);
+        var domain = p.domain;
+        domain.remove('a');
+        expect(p.image('a'), equals('a'));
+        expect(p.domain, equals(['a']));
+      });
+    });
+  });
+}
